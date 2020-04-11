e4efa85fd852cec3739678547b9ed4264af6a7b0
mics <michal.strbka@gmail.com>
2020-04-11 22:36:57 +0200

    19: Document Renderer: default page setup

--- Source/Sidea.DocxToPdf/Renderers/Documents/DocumentRenderer.cs
+++ Source/Sidea.DocxToPdf/Renderers/Documents/DocumentRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using DocumentFormat.OpenXml.Packaging;
+using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 
 namespace Sidea.DocxToPdf.Renderers.Documents
@@ -8,10 +9,29 @@ namespace Sidea.DocxToPdf.Renderers.Documents
         public PdfDocument Render(WordprocessingDocument docx)
         {
             var pdf = new PdfDocument();
-            var page = pdf.AddPage();
-            page.Size = PdfSharp.PageSize.A4;
+            var page = this.CreatePage();
+            pdf.AddPage(page);
 
             return pdf;
+        }
+
+        private PdfPage CreatePage()
+        {
+            var margin = XUnit.FromCentimeter(2.5);
+
+            var page = new PdfPage
+            {
+                Size = PdfSharp.PageSize.A4,
+                TrimMargins = new TrimMargins
+                {
+                    Top = margin,
+                    Right = margin,
+                    Bottom = margin,
+                    Left = margin,
+                }
+            };
+
+            return page;
         }
     }
 }
