35a9cfbeea4ab07786179923ffe7c0111b734352
mics <michal.strbka@gmail.com>
2020-04-24 10:05:17 +0200

    50: Image

---
+++ Source/Sidea.DocxToPdf.Tests/ImagesTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sidea.DocxToPdf.Tests
+{
+    [TestClass]
+    public class ImagesTests : TestBase
+    {
+        public ImagesTests() : base("Images")
+        {
+        }
+
+        [TestMethod]
+        public void Image()
+        {
+            this.Generate(nameof(Image));
+        }
+    }
+}
