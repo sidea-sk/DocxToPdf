362beb5f195e1a8de9faad67382272d35006aead
mics <michal.strbka@gmail.com>
2020-05-14 14:03:36 +0200

    Changed api

--- Source/Sidea.DocxToPdf/PdfGenerator.cs
+++ Source/Sidea.DocxToPdf/PdfGenerator.cs
@@ -14,7 +14,21 @@ namespace Sidea.DocxToPdf
             return pdf;
         }
 
-        public PdfDocument Generate(WordprocessingDocument docx, RenderingOptions options = null)
+        public MemoryStream GenerateAsStream(Stream docxStream, RenderingOptions options = null)
+        {
+            var pdf = this.Generate(docxStream, options);
+            var ms = new MemoryStream();
+            pdf.Save(ms);
+            return ms;
+        }
+
+        public byte[] GenerateAsByteArray(Stream docxStream, RenderingOptions options = null)
+        {
+            using var ms = this.GenerateAsStream(docxStream, options);
+            return ms.ToArray();
+        }
+
+        private PdfDocument Generate(WordprocessingDocument docx, RenderingOptions options = null)
         {
             var renderingOptions = options ?? RenderingOptions.Default;
             var renderer = new DocumentRenderer(docx, renderingOptions);
