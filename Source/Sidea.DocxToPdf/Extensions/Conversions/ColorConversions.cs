4e6c2a585afa2080799a5389c55fbe425fa77821
mics <michal.strbka@gmail.com>
2020-07-07 10:51:31 +0200

    Fixed highlight colors

--- Source/Sidea.DocxToPdf/Extensions/Conversions/ColorConversions.cs
+++ Source/Sidea.DocxToPdf/Extensions/Conversions/ColorConversions.cs
@@ -3,20 +3,16 @@ using System.Globalization;
 using OpenXml = DocumentFormat.OpenXml;
 using Word = DocumentFormat.OpenXml.Wordprocessing;
 
+using static DocumentFormat.OpenXml.Wordprocessing.HighlightColorValues;
+
 namespace Sidea.DocxToPdf
 {
     internal static class ColorConversions
     {
         public static Color ToColor(this Word.Highlight highlight)
         {
-            var colorName = highlight?.Val ?? Word.HighlightColorValues.None;
-            
-            if(colorName == Word.HighlightColorValues.None)
-            {
-                return Color.Empty;
-            }
-
-            var c = Color.FromName(colorName.InnerText);
+            var colorName = highlight?.Val ?? None;
+            var c = colorName.Value.ToColor();
             return c;
         }
 
@@ -54,6 +50,31 @@ namespace Sidea.DocxToPdf
             var g = int.Parse(hex.Substring(2, 2), NumberStyles.HexNumber);
             var b = int.Parse(hex.Substring(4, 2), NumberStyles.HexNumber);
             return (r, g, b);
+        }
+
+        private static Color ToColor(this Word.HighlightColorValues name)
+        {
+            return name switch
+            {
+                Black => Color.FromArgb(0, 0, 0),
+                Blue => Color.FromArgb(0, 0, 0xFF),
+                Cyan => Color.FromArgb(0, 0xFF, 0xFF),
+                Green => Color.FromArgb(0, 0xFF, 0),
+                Magenta => Color.FromArgb(0xFF, 0, 0xFF),
+                Red => Color.FromArgb(0xFF, 0, 0),
+                Yellow => Color.FromArgb(0xFF, 0xFF, 0),
+                White => Color.FromArgb(0xFF, 0xFF, 0xFF),
+                DarkBlue => Color.FromArgb(0, 0, 0x80),
+                DarkCyan => Color.FromArgb(0, 0x80, 0x80),
+                DarkGreen => Color.FromArgb(0, 0x80, 0),
+                DarkMagenta => Color.FromArgb(0x80, 0, 0x80),
+                DarkRed => Color.FromArgb(0x80, 0, 0),
+                DarkYellow => Color.FromArgb(0x80, 0x80, 0),
+                DarkGray => Color.FromArgb(0x80, 0x80, 0x80),
+                LightGray => Color.FromArgb(0xC0, 0xC0, 0xC0),
+                None => Color.Empty,
+                _ => Color.Empty
+            };
         }
     }
 }
